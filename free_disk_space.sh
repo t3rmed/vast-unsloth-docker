b13b6483b8bec01e566bd729b2085fead15f149f
termed <termed@robbery.gg>
2025-12-16 15:59:20 -0700

    Create free_disk_space.sh

---
+++ free_disk_space.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+echo "=============================================================================="
+echo "Freeing up disk space on CI system for Docker Build"
+echo "=============================================================================="
+
+df -h
+echo "Removing large packages..."
+
+# Remove large development/SDK packages (GHC, Dotnet, LLVM, PHP)
+sudo apt-get remove -y '^ghc-8.*'
+sudo apt-get remove -y '^dotnet-.*'
+sudo apt-get remove -y '^llvm-.*'
+sudo apt-get remove -y 'php.*'
+
+# Remove large SDKs and Browsers, adding monodoc-http before mono-devel as suggested
+sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox monodoc-http powershell mono-devel
+
+sudo apt-get autoremove -y
+sudo apt-get clean
+df -h
+
+echo "Removing large directories (e.g., /usr/share/dotnet/)..."
+# deleting approx 15GB
+rm -rf /usr/share/dotnet/
+df -h
+echo "=============================================================================="
